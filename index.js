59ed1b612a0e684d406cccb9c9323452b78ad324
Your Name <superzackx@gmail.com>
2023-07-26 12:21:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+
+const port = 3000;
+app.set("view engine" , "ejs")
+
+app.get("/" , (req, res) => {
+    res.render("index")
+})
+
+app.get("/about" , (req, res) => {
+    res.render("about")
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
